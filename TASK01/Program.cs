265a824a429e4e181c943ed6356ed7f4100423d7
valera mironov <valera4944690@mail.ru>
2021-11-04 21:15:27 +0300

    .\TASK01\

---
+++ TASK01/Program.cs
@@ -0,0 +1,15 @@
+﻿double a;
+double b;
+Console.Write("Введите число a:");
+string s = Console.ReadLine();
+a=Convert.ToInt32(s);
+
+Console.Write("Введите число b:");
+s = Console.ReadLine();
+b=Convert.ToInt32(s);
+if(a == b*b){
+Console.WriteLine("Являеться");
+}
+else{
+Console.WriteLine("Не являеться");
+}
